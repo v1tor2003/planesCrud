b0bf880484d46f5b92a6b911d08ee97705728ae0
v1tor2003 <vitor.pr04@hotmail.com>
2022-09-30 17:08:49 -0300

    initial-version

---
+++ api/api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors") 
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+app.use("/auth", require("./routes/auth"));
+app.use("/posts", require("./routes/posts"));
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
